50f19d02138aa4814ab5c537dde6755a60267ec0
Animesh Basak <animeshbasak@Animeshs-MacBook-Pro.local>
2022-02-02 01:03:03 +0530

    updating our page with shop page

--- src/App.js
+++ src/App.js
@@ -2,43 +2,13 @@ import React from "react";
 import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-const Jackets = () => (
-  <div>
-    <h1>Jackets PAGE</h1>
-  </div>
-);
-const Sneakers = () => (
-  <div>
-    <h1>Sneakers PAGE</h1>
-  </div>
-);
-const Womens = () => (
-  <div>
-    <h1>Womens PAGE</h1>
-  </div>
-);
-const Mens = () => (
-  <div>
-    <h1>Mens PAGE</h1>
-  </div>
-);
-
+import ShopPage from "./pages/shop/shop.component.jsx";
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
-        <Route path="/shop/jackets" component={Jackets} />
-        <Route path="/shop/sneakers" component={Sneakers} />
-        <Route path="/shop/womens" component={Womens} />
-        <Route path="/shop/mens" component={Mens} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
